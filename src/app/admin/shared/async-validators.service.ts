2cf029f599f6e225ca47cf2b496397baf8c1b801
The Buildbot <buildbot@angular2buch.de>
2022-08-02 05:11:25 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/242f66f246fe6f5a617571243b0e9f566f2ed655

--- src/app/admin/shared/async-validators.service.ts
+++ src/app/admin/shared/async-validators.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
-/*BS*/import { AsyncValidatorFn } from '@angular/forms';
+import { AsyncValidatorFn } from '@angular/forms';
 import { map } from 'rxjs';
 
-import { BookStoreService } from '../../shared/book-store.service';/*BE*/
+import { BookStoreService } from '../../shared/book-store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsyncValidatorsService {
 
-  constructor(/*BS*/private service: BookStoreService/*BE*/) { }
+  constructor(private service: BookStoreService) { }
 
-  /*BS*/isbnExists(): AsyncValidatorFn {
+  isbnExists(): AsyncValidatorFn {
     return (control) => {
       return this.service.check(control.value).pipe(
         map(exists => exists ? { isbnexists: true } : null)
       );
     }
-  }/*BE*/
+  }
 }
